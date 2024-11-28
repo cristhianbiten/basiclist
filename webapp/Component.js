498ec9035461cf69191be31286462b1d37f8663b
Cristhian Bitencourt <cristhian.bitencourt@fusionconsultoria.com>
2024-11-28 01:27:11 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("basiclist.student39.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
